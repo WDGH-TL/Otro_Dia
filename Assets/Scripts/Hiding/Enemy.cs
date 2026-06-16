2ff58638cc523f67f4de7b96820f28ed299b3f81
karynherrera <karynherrera@gmail.com>
2026-06-15 23:38:11 -0400

    movimiento enemy 1 punto
    movimiento enemy 1 punto

--- Assets/Scripts/Hiding/Enemy.cs
+++ Assets/Scripts/Hiding/Enemy.cs
@@ -6,9 +6,26 @@ public class Enemy : MonoBehaviour
 {
     private bool hasEnemy = false;
 
+    public Transform punto1;
+    public Transform punto2;
+    public float velocidad = 2f;
+
     void Start()
     {
+        if (punto1 != null)
+        {
+            transform.position = punto1.position;
+        }
         StartCoroutine(EnemyTimer());
+    }
+
+    void Update()
+    {
+       
+        if (punto2 != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, punto2.position, velocidad * Time.deltaTime);
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -41,7 +58,7 @@ public class Enemy : MonoBehaviour
 
     public IEnumerator EnemyTimer()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(8f);
         gameObject.SetActive(false);
 
     }
